fe5f2812ce97d34cebe299b5118edca2fa4213a5
NguyenVuKhang <brew4k@gmail.com>
2021-05-17 18:08:14 +0800

    removed mem, batt reload every two seconds

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval(seconds)*/	/*Update Signal*/
-	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	// {"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
     {"Vol: ", "get_volume.sh",	0,	10},
-    {"Batt: ", "get_battery.sh",	0,	10},
+    {"Batt: ", "get_battery.sh",	2,	0},
 	{"", "date '+%a, %-d %b - %R'"                  			   ,	30,		0},
 };
 
