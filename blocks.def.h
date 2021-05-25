77fe772998bf1c6fa453a03832ab34e71ac84a59
nguyenvukhang <brew4k@gmail.com>
2021-05-25 15:33:27 +0800

    separated time and date and added manual delimiters

--- blocks.def.h
+++ blocks.def.h
@@ -2,11 +2,15 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval(seconds)*/	/*Update Signal*/
 	// {"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-    {"Vol: ",  "~/.local/bin/get_volume.sh",  0, 10},
-    {"Batt: ", "~/.local/bin/get_battery.sh", 2,  0},
-	{"",       "~/.local/bin/get_date.sh",   30,  0},
+    {"",  "~/.local/bin/get_volume.sh",          0,  10},
+    {"〈  ",  "", 0, 0},
+    {"", "~/.local/bin/get_battery.sh",          2,   0},
+    {"〈  ",  "", 0, 0},
+	{"",       "~/.local/bin/get_date.sh",   28800,   0},
+    {"〈  ",  "", 0, 0},
+	{"",       "~/.local/bin/get_time.sh",      30,   0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
